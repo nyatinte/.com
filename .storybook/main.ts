b73ae635ca241950822c8a3d7b44666883b5023e
nyatinte <takumi.062757@gmail.com>
2025-11-15 22:09:11 +0900

    pnpm create storybook@latest

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+const config: StorybookConfig = {
+  stories: ["../app/**/*.stories.tsx", "../components/**/*.stories.tsx"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest",
+  ],
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
